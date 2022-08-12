dbea1c689657e0cbdf206fee7f18cf4950a77b10
Max-Degtyarev <Usamaximum@mail.ru>
2022-08-12 22:00:17 +0300

    BinaryTree

---
+++ BinaryTree/mian.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+#define delimiter "\n-------------------------------------------\n"
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+
+
+
+
+}
